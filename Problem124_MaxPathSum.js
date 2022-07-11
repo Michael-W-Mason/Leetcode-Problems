d0c4e031cd0aa565f7169f22e79a4454930636f7
Michael Mason <mwmason1998@gmail.com>
2022-07-11 09:20:31 -0400

    Add 2 Numbers

--- Problem124_MaxPathSum.js
+++ Problem124_MaxPathSum.js
@@ -8,16 +8,8 @@
  */
 /**
  * @param {TreeNode} root
- * @return {TreeNode}
+ * @return {number}
  */
- var invertTree = function (root) {
-    if(root === null){
-        return null;
-    }
-    temp = root.left;
-    root.left = root.right;
-    root.right = temp;
-    invertTree(root.left);
-    invertTree(root.right);
-    return root;
+var maxPathSum = function (root) {
+    
 };
